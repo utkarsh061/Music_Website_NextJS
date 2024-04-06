b5c117de8279c729c081c5ab6b685ebfe3ac5cdd
Utkarsh Garg <gargu81@gmail.com>
2024-04-06 18:25:11 +0530

    "Testimonial & Why choose us Section"

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function courses() {
+  return (
+    <div>Utkarsh Garg</div>
+  );
+}
+ 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +9,9 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
       <FeaturedSection/>
+      <WhyChooseUs/>
+      <MusicSchoolTestimonials/>
     </main>
   );
 }
+ 
